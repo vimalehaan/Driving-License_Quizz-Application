2882ec80a1ca94c2918b443f8dbf168fd76c2109
vimalehaan <vimalehaan2312@mail.com>
2024-02-20 23:01:21 +0530

    Add Test Page Styled (i)

--- src/AddTest.js
+++ src/AddTest.js
@@ -36,12 +36,13 @@ import {
   Chip
 } from './Mui'
 import SideBar from "./Components/Admin/SideBar";
+import { TopButtons } from "./Components/Admin/TopButtons";
 
 function AddTest() {
 
   const [activeButton, setActiveButton] = useState({ addQuestions: false, addAnswers: false, correctAnswer: false, timeSetting: false, removeTest: false });
 
-  const handleButtonClick = (id) => {                                        
+  const handleButtonClick = (id) => {
     setActiveButton((prevState) => ({                                       //Set a button active at a time..
       ...Object.fromEntries(Object.keys(prevState).map(key => [key, key === id ? true : false]))
     }));
@@ -58,25 +59,25 @@ function AddTest() {
       >
         <Grid container spacing='20px' sx={{ marginTop: '30px' }}>
           <Grid item lg='12' sx={{ marginBottom: '45px' }}>
-            <ButtonGroup  sx={{ display: 'flex', justifyContent: 'left', width: '750px', borderRadius: '20px', backgroundColor: '#F0F2F7' }} >
-              <CusButton sx={{ height: '40px', width: '250px' }}>hello</CusButton>
-              <CusButton sx={{ height: '40px', width: '250px' }}>hello</CusButton>
-              <CusButton sx={{ height: '40px', width: '250px' }}>hello</CusButton>
-            </ButtonGroup>
+            <Box sx={{ display: 'flex', justifyContent: 'left', width: '748px', borderRadius: '20px', backgroundColor: '#F0F2F7' }}>
+              <TopButtons buttons={['Tests', 'Car', 'Commercial Vehicle', 'Motorbike']} sx={{ width: '250px' }} />
+              <TopButtons buttons={['Difficulty', 'Easy', 'Hard']}/>
+              <TopButtons buttons={['User Type', 'Normal', 'Premium']}/>
+            </Box>
           </Grid>
 
           <Grid item lg='3.8' xs='0'>
             <Item elevation={0} sx={{ height: '270px', marginBottom: '23px' }}>
               <Stack direction={"column"} spacing={3}>
-                <CusButton variant="outlined" disableTouchRipple sx={{width: '300px', height: '50px',}}
+                <CusButton variant="outlined" disableTouchRipple sx={{ width: '300px', height: '50px', }}
                   clicked={activeButton['addQuestions']}
                   onClick={() => handleButtonClick('addQuestions')}
                   startIcon={<AddCircleOutlinedIcon />}>Questions</CusButton>
-                <CusButton variant="outlined" disableTouchRipple sx={{width: '300px', height: '50px',}}
+                <CusButton variant="outlined" disableTouchRipple sx={{ width: '300px', height: '50px', }}
                   clicked={activeButton['addAnswers']}
                   onClick={() => handleButtonClick('addAnswers')}
                   startIcon={<AddCircleOutlinedIcon />}>Answers</CusButton>
-                <CusButton variant="outlined" disableTouchRipple sx={{width: '300px', height: '50px',}}
+                <CusButton variant="outlined" disableTouchRipple sx={{ width: '300px', height: '50px', }}
                   clicked={activeButton['correctAnswer']}
                   onClick={() => handleButtonClick('correctAnswer')}
                   startIcon={<AddCircleOutlinedIcon />}>Correct Answer</CusButton>
@@ -85,11 +86,11 @@ function AddTest() {
 
             <Item elevation={0} sx={{ height: '200px' }}>
               <Stack direction={"column"} spacing={3}>
-                <CusButton variant="outlined" sx={{width: '300px', height: '50px',}}
+                <CusButton variant="outlined" sx={{ width: '300px', height: '50px', }}
                   clicked={activeButton['timeSetting']}
                   onClick={() => handleButtonClick('timeSetting')}
                   disableTouchRipple startIcon={<AddCircleOutlinedIcon />}>Time Setting</CusButton>
-                <CusButton variant="outlined" sx={{width: '300px', height: '50px',}}
+                <CusButton variant="outlined" sx={{ width: '300px', height: '50px', }}
                   clicked={activeButton['removeTest']}
                   onClick={() => handleButtonClick('removeTest')}
                   disableTouchRipple startIcon={<AddCircleOutlinedIcon />}>Remove Test</CusButton>
@@ -99,8 +100,8 @@ function AddTest() {
 
           <Grid item lg='8' xs='12'>
             <Item elevation={0} sx={{ height: '493px' }}>
-              <Stack direction={'column'} spacing={1} sx={{marginTop: '-35px'}}>
-                
+              <Stack direction={'column'} spacing={1} sx={{ marginTop: '-35px' }}>
+
                 {/* <Item elevation={0} sx={{ height: '493px' }}>
                   {switchCompo(activeButton)}
                 </Item> */}
