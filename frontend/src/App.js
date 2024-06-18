58aa9a635c34c513d4f722255e8c6d0fcd7fb712
vimalehaan <vimalehaan2312@mail.com>
2024-06-18 13:26:27 +0530

    Routes Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddTest from './Pages/AdminPage/AddTest';
-import Payment from './Pages/PaymentPage/Payment';
 import QuestionTextField from './Components/Admin/QuestionTextField';
 import TestIdComponent from './Components/Admin/TestIDContainer';
 import CarExamDashboard from './Pages/QuizzDashboardPage/CarExamDashboard';
@@ -13,10 +13,30 @@ import BasicPie from './Components/UserProfile/PieChart';
 import PassRatioChart from './Components/UserProfile/PassRatioChart';
 import StickyHeadTable from './Components/UserProfile/ExamTable';
 import ViewResultPage from './Pages/ViewResult';
+// import CheckoutForm from './Components/Payment/CheckoutForm';
+// import Return from './Components/Payment/Return';
+
 
 function App() {
   return (
     <div className="App">
+
+    <Router>
+      <Routes>
+        <Route path="/loginpg1" element={<Loginpg1 />} />
+        <Route path="/loginpg2" element={<Loginpg2 />} />
+        <Route path="/addtest" element={<AddTest />} />
+        <Route path="/customdialog" element={<CustomizedDialogs />} />
+        <Route path="/carexamdb" element={<CarExamDashboard />} />
+        <Route path="/userpropage" element={<UserProfilePage />} />
+        <Route path="/piechart" element={<BasicPie />} />
+        <Route path="/passratiochart" element={<PassRatioChart />} />
+        <Route path="/stickyheadtable" element={<StickyHeadTable />} />
+        <Route path="/result" element={<ViewResultPage />} />
+        {/* <Route path="/checkout" element={<CheckoutForm />} /> */}
+        {/* <Route path="/return" element={<Return />} /> */}
+      </Routes>
+    </Router>
       
       {/* <Loginpg1 /> */}
       {/* <Loginpg2 /> */}
@@ -25,7 +45,10 @@ function App() {
       {/* <Payment /> */}
       {/* <CarExamDashboard /> */}
       {/* <UserProfilePage /> */}
-      <ViewResultPage />
+      {/* <BasicPie /> */}
+      {/* <PassRatioChart /> */}
+      {/* <StickyHeadTable /> */}
+
     </div>
   );
 }
