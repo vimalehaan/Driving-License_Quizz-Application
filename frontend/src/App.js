60675069346f13510b3d05c989395b6091f5121a
SharujaB25 <sharuja.balendrakumar@gmail.com>
2024-06-20 14:06:00 +0530

    authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,10 @@ import TestIdComponent from './Components/Admin/TestIDContainer';
 import CarExamDashboard from './Pages/QuizzDashboardPage/CarExamDashboard';
 import Login from './Pages/UserLogPage/Login';
 import SocialSignIn from './Pages/UserLogPage/SocialSignIn';
+import Signup from './Pages/UserLogPage/Signup';
 import OTP from './Pages/UserLogPage/OTP';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ResetPassword from './Pages/UserLogPage/ResetPassword';
 function App() {
   return (
     <div className="App">
@@ -15,6 +17,9 @@ function App() {
         <Routes>
           <Route exact path="/login" element={<Login/>}/>
           <Route exact path="/social-login" element={<SocialSignIn/>}></Route>
+          <Route exact path="/signup" element={<Signup/>}></Route>
+          <Route exact path="/reset" element={<ResetPassword/>}></Route>
+          
           {/* <Route path="/a"><AddTest /></Route>
           <Route path="/b"><Payment /></Route>
           <Route path="/c"><CarExamDashboard /></Route>
