59e4c0477f22f7091da00472a610fc5e3cd307ee
Aranya2402 <aranya24arii@gmail.com>
2024-07-01 16:25:54 +0530

    Banujan Invoice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,25 +4,25 @@ import Quizdashboard from './Components/Admin/AddQuestion.js/quizDashboard'
 import ViewQuestions from './Pages/AdminPage/ViewQuestions'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import AddTest from './Pages/AdminPage/AddTest';
-import QuestionTextField from './Components/Admin/QuestionTextField';
-import TestIdComponent from './Components/Admin/TestIDContainer';
-import CarExamDashboard from './Pages/QuizzDashboardPage/CarExamDashboard';
-import CustomizedDialogs from './Components/Admin/SaveDialog';
-import NameCard from './Components/UserProfile/NameCard'
-import UserProfilePage from './Pages/UserProfile'
-import BasicPie from './Components/UserProfile/PieChart';
-import PassRatioChart from './Components/UserProfile/PassRatioChart';
-import StickyHeadTable from './Components/UserProfile/ExamTable';
-import ViewResultPage from './Pages/ViewResult';
-import CheckoutForm from './Components/Payment/CheckoutForm';
-import Return from './Components/Payment/Return';
-import Login from './Pages/UserLogPage/Login';
-import SocialSignIn from './Pages/UserLogPage/SocialSignIn';
-import Signup from './Pages/UserLogPage/Signup';
-import ResetPassword from './Pages/UserLogPage/ResetPassword';
-import AttemptQuiz from './Pages/AttemptQuiz';
-
+// import AddTest from './Pages/AdminPage/AddTest';
+// import QuestionTextField from './Components/Admin/QuestionTextField';
+// import TestIdComponent from './Components/Admin/TestIDContainer';
+// import CarExamDashboard from './Pages/QuizzDashboardPage/CarExamDashboard';
+// import CustomizedDialogs from './Components/Admin/SaveDialog';
+// import NameCard from './Components/UserProfile/NameCard'
+// import UserProfilePage from './Pages/UserProfile'
+// import BasicPie from './Components/UserProfile/PieChart';
+// import PassRatioChart from './Components/UserProfile/PassRatioChart';
+// import StickyHeadTable from './Components/UserProfile/ExamTable';
+// import ViewResultPage from './Pages/ViewResult';
+// import CheckoutForm from './Components/Payment/CheckoutForm';
+// import Return from './Components/Payment/Return';
+// import Login from './Pages/UserLogPage/Login';
+// import SocialSignIn from './Pages/UserLogPage/SocialSignIn';
+// import Signup from './Pages/UserLogPage/Signup';
+// import ResetPassword from './Pages/UserLogPage/ResetPassword';
+// import AttemptQuiz from './Pages/AttemptQuiz';
+import Invoice from './Pages/InvoicePage/Invoice';
 
 
 function App() {
@@ -30,7 +30,7 @@ function App() {
     <div className="App">
     <Router>
       <Routes>
-        <Route exact path="/login" element={<Login/>}/>
+        {/* <Route exact path="/login" element={<Login/>}/>
         <Route exact path="/social-login" element={<SocialSignIn/>}></Route>
         <Route exact path="/signup" element={<Signup/>}></Route>
         <Route exact path="/reset" element={<ResetPassword/>}></Route>
@@ -47,9 +47,8 @@ function App() {
         <Route path="/return" element={<Return />} />
         <Route path="/quiz" element={<AttemptQuiz />} />
         <Route path="/questions" element={<ViewQuestions />} />
-        <Route path="/quiz-dashboard" element={<Quizdashboard />} />
-
-
+        <Route path="/quiz-dashboard" element={<Quizdashboard />} /> */}
+        <Route path="/invoice" element={<Invoice />} />
       </Routes>
     </Router>
 
